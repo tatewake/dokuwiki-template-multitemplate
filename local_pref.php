b3f624508abf3c5f3e3ba6fb958020fa14c11df1
Terence J. Grant <tjgrant@tatewake.com>
2009-10-11 02:15:46 -0500

    first commit

---
+++ local_pref.php
@@ -0,0 +1,11 @@
+<?php
+/** Multitemplate for DokuWiki
+ * By Terence J. Grant
+ * tjgrant [at] tatewake [dot] com
+ * License: GPL v2
+ */
+
+//$multitemplate['playground:playground'] = 'default';
+$multitemplate[''] = 'monobook';
+
+?>
